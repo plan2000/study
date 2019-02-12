dfd4d62f300498f23df330bc8e317c7e733ad27b
plan2000 <plan2000@plan2000-PC>
2019-02-12 15:55:59 +0900

    zdsfsadf

--- samsung/samsunglifeCore/src/main/java/com/samsunglife/sample/core/SamsungConst.java
+++
@@ -1,7 +0,0 @@
-package com.samsunglife.sample.core;
-
-public class SamsungConst {
-
-	public static String sessionKeyName = "SAMSUNGSESSION";
-	public static String enabler = "securityEnabler";
-}
